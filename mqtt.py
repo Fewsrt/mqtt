d7e10c387227db66d5736e51352254d2e9dde9b5
fewsrt <chonnaphat.v@gmail.com>
2020-03-24 19:42:49 +0700

    mqtt-1

--- mqtt.py
+++ mqtt.py
@@ -1,30 +1,48 @@
-import paho.mqtt.client as mqtt 
-import RPi.GPIO as GPIO 
-import time 
-import thread
-# ————– # # Board settings # # ————– # 
-GreenPin =23 
-GPIO.setwarnings(False) GPIO.setmode(GPIO.BOARD) 
-# use P1 header pin numbering convention 
-GPIO.cleanup() 
-# clean up resources 
-GPIO.setup(GreenPin, GPIO.OUT) 
-# led pin setup
-# The callback for when the client receives a CONNACK response from the server. 
-def on_connect(client, userdata, flags, rc): 
-print(“Connected with result code “+str(rc)) 
-# Subscribing in on_connect() means that if we lose the connection and # reconnect then subscriptions will be renewed. 
-GPIO.output(GreenPin,GPIO.HIGH) 
-client.subscribe(“light-1”) 
-def on_message(client, userdata, msg):
- payload = str(msg.payload) topic = str(msg.topic) 
-print(msg.topic + ” Payload is ” + payload) 
-if payload == ‘off’: GPIO.output(GreenPin,GPIO.HIGH) 
-elif payload == “on”: GPIO.output(GreenPin, GPIO.LOW) 
-else: 
-print(“WRONG MESSAGE”) 
-client = mqtt.Client(transport=”websockets”) 
-client.on_connect = on_connect 
-client.on_message = on_message
-client.connect(“fasacserver.ddns.net”, 9001, 60)
-client.loopstart()
+import paho.mqtt.client as mqttClient
+import time
+ 
+def on_connect(client, userdata, flags, rc):
+ 
+    if rc == 0:
+ 
+        print("Connected to broker")
+ 
+        global Connected                #Use global variable
+        Connected = True                #Signal connection 
+ 
+    else:
+ 
+        print("Connection failed")
+ 
+def on_message(client, userdata, message):
+    print "Message received: "  + message.payload
+ 
+Connected = False   #global variable for the state of the connection
+ 
+broker_address= "fasacserver.ddns.net"  #Broker address
+port = 1883                        #Broker port
+user = "fasac"                    #Connection username
+password = "Fasac050800"            #Connection password
+ 
+client = mqttClient.Client("Python")               #create new instance
+client.username_pw_set(user, password=password)    #set username and password
+client.on_connect= on_connect                      #attach function to callback
+client.on_message= on_message                      #attach function to callback
+ 
+client.connect(broker_address, port=port)          #connect to broker
+ 
+client.loop_start()        #start the loop
+ 
+while Connected != True:    #Wait for connection
+    time.sleep(0.1)
+ 
+client.subscribe("python/test")
+ 
+try:
+    while True:
+        time.sleep(1)
+ 
+except KeyboardInterrupt:
+    print "exiting"
+    client.disconnect()
+    client.loop_stop()
