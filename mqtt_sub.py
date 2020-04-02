5e9f17c595aca02c54f7751435dd2c3fd09d8f1a
fewsrt <chonnaphat.v@gmail.com>
2020-04-02 21:41:53 +0700

    pub

--- mqtt_sub.py
+++ mqtt_sub.py
@@ -12,44 +12,46 @@ LED1 = 23
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(LED1, GPIO.OUT)
 try:
-  def on_connect(self,mosq, obj, rc):
-     mqttc.subscribe(MQTT_TOPIC, 0)
-     print("Connect on "+MQTT_HOST)
-  def on_message(mosq, obj, msg):
-     if (msg.payload=='1'):
-           GPIO.output(LED1,True)
-           mqttc.publish("relay/on","ON")
-           print 'lamp aan'
-           print "Topic: " + str(msg.topic)
-           print "QoS: " + str(msg.qos)
-     if (msg.payload=='0'):
-           GPIO.output(LED1,False)
-           mqttc.publish("relay/off","OFF")
-           print 'lamp uit'
-           print "Topic: " + str(msg.topic)
-           print "QoS: " + str(msg.qos)
+    def on_connect(self, mosq, obj, rc):
+        mqttc.subscribe(MQTT_TOPIC, 0)
+        print("Connect on "+MQTT_HOST)
 
-  def on_subscribe(mosq, obj, mid, granted_qos):
-          print("Subscribed to Topic: " + 
-          MQTT_TOPIC + " with QoS: " + str(granted_qos))
+    def on_message(mosq, obj, msg):
+        if (msg.payload == '1'):
+            GPIO.output(LED1, True)
+            mqttc.publish("relay/on", "ON")
+            print 'lamp aan'
+            print "Topic: " + str(msg.topic)
+            print "QoS: " + str(msg.qos)
+        if (msg.payload == '0'):
+            GPIO.output(LED1, False)
+            mqttc.publish("relay/off", "OFF")
+            print 'lamp uit'
+            print "Topic: " + str(msg.topic)
+            print "QoS: " + str(msg.qos)
 
-    # Initiate MQTT Client
-  mqttc = mqtt.Client()
+    def on_subscribe(mosq, obj, mid, granted_qos):
+        print("Subscribed to Topic: " +
+              MQTT_TOPIC + " with QoS: " + str(granted_qos))
 
-    # Assign event callbacks
-  mqttc.on_message = on_message
-  mqttc.on_connect = on_connect
-  mqttc.on_subscribe = on_subscribe
+  # Initiate MQTT Client
+    mqttc = mqtt.Client()
 
-    # Connect with MQTT Broker
-  mqttc.connect(MQTT_HOST, MQTT_PORT, MQTT_KEEPALIVE_INTERVAL)
+  # Assign event callbacks
+    mqttc.on_message = on_message
+    mqttc.on_connect = on_connect
+    mqttc.on_subscribe = on_subscribe
+    mqttc.on_publish = on_publish
 
-    # Continue monitoring the incoming messages for subscribed topic
-  mqttc.loop_forever()
+  # Connect with MQTT Broker
+    mqttc.connect(MQTT_HOST, MQTT_PORT, MQTT_KEEPALIVE_INTERVAL)
 
-except KeyboardInterrupt:  
-    # here you put any code you want to run before the program   
-    # exits when you press CTRL+C
-    GPIO.cleanup()  
-#finally:  
-    #GPIO.cleanup() # this ensures a clean exit 
+  # Continue monitoring the incoming messages for subscribed topic
+    mqttc.loop_forever()
+
+except KeyboardInterrupt:
+  # here you put any code you want to run before the program
+  # exits when you press CTRL+C
+    GPIO.cleanup()
+# finally:
+    # GPIO.cleanup() # this ensures a clean exit
