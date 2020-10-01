5e229b9932bbcbaa7580747ff9fab57c6d43c586
FaSAC-Auto <chonnaphat.v@gmail.com>
2020-10-01 23:32:59 +0700

    up

--- mqtt_pub_client.py
+++ mqtt_pub_client.py
@@ -27,6 +27,10 @@ def publish_message(topic, message):
 
     publish.single(topic, message, hostname="192.168.88.220")
 
+temp = get_temp()
+serial = getserial()
+print(serial)
+
 while True:
     temp = get_temp()
     serial = getserial()
